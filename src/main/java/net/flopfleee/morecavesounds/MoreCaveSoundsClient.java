08949ca17392ecd966b153a7520dfdd733783817
Prometheus786 <mohammed.khan@jhgs.bucks.sch.uk>
2024-11-29 12:21:13 +0000

    Initial commit

---
+++ src/main/java/net/flopfleee/morecavesounds/MoreCaveSoundsClient.java
@@ -0,0 +1,11 @@
+package net.flopfleee.morecavesounds;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreCaveSoundsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
